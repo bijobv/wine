55cf9f2ca673d685f170bcfd0148328fdfa871ba
bijobv <bijobv@gmail.com>
2024-06-01 17:08:38 +1000

    Version 2 for Streamlit

--- WineVisualApp.py
+++ WineVisualApp.py
@@ -325,9 +325,9 @@ st.plotly_chart(fig)
 ########### 1C ############
 st.write("<br>" * 2, unsafe_allow_html=True)
 st.subheader('C. Predicting Quality Values')
-st.markdown("""This visualisation has been produced, displaying the quality scores and the associated attribute values of all wines in the dataset, 
-            across the 11-dimensional feature vectors. It can be used to predict a quality value given an unseen data sample across those 11 dimensions. 
-            The visualisation aims to assist the viewer to accurately predict the quality value based on the random data sample.""")
+st.markdown("""You can use this tool to predict the likely score of a wine given some all of its attributes. The more attributes you use the more focused the prediction will be.
+            Pin a specific value, or rage of values, on one or multiple attributes to determine the probable quality score of the wine.
+            Note that with some attributes fixed, you can change the pins on other attributes to see the outcome on the probable wine score.""")
 
 df = pd.concat([data1, data2], ignore_index=True)
 
